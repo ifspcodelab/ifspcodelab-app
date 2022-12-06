181772012e062bf0f98c0cb7958507673edb277b
Domingos Latorre <domingos.latorre@gmail.com>
2022-12-06 16:00:18 -0300

    first commit

---
+++ src/test/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/AppTest.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
