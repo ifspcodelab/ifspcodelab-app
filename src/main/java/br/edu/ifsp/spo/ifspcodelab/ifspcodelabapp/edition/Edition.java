24928e287b3cb3f1c82f5a6c206db2d42812a1d0
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-12-07 16:28:33 -0300

    feat: add edition migration

--- src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/edition/Edition.java
+++ src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/edition/Edition.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Table(name = "subevents")
+@Table(name = "editions")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
--- src/main/resources/db/migration/V1__create_projects_table.sql
+++ src/main/resources/db/migration/V1__create_projects_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE projects(
     id UUID NOT NULL,
     name VARCHAR NOT NULL UNIQUE,
-    description VARCHAR NOT NULL
+    description VARCHAR NOT NULL,
+    CONSTRAINT projects_pk PRIMARY KEY (id)
 )
