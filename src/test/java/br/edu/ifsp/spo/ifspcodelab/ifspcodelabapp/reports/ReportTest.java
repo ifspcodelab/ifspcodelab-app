641f908546f7b41be88a5f6b5676f8d08d08823a
Domingos Latorre <domingos.latorre@gmail.com>
2022-12-07 10:32:53 -0300

    test: fix break signatures block

--- src/test/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/reports/ReportTest.java
+++ src/test/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/reports/ReportTest.java
@@ -1,6 +1,5 @@
 package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -103,6 +102,23 @@ public class ReportTest {
             false
         );
         var fos = new FileOutputStream("scholarshipMonthlyReport.pdf");
+        fos.write(MonthlyReportPdf.generate(data));
+        fos.close();
+    }
+
+    @Test
+    public void monthlyReportBreakSignatures() throws Exception  {
+        var data = new MonthlyReportData(
+            "Laboratório de Desenvolvimento de Software – IFSP CodeLab", 
+            "João da Silva", 
+            "Maria Campus Gomes", 
+            LocalDate.now(), 
+            "Lorem ipsum dolor sit amet quis consectetur est. Nunc id enim sed magna efficitur volutpat sit amet ac ex. sed magna efficitur volutpat sit amet ac ex. sed magna efficitur volutpat sit amet ac ex. ",
+            "Vestibulum accumsan id metus sit amet biestibulum quis nisl eget, cursus pretium  Nunc id enim sed magna efficitur v orci. Nunc id enim sed magna efficitur volutpat sit amet ac ex. ",
+            "Viverra libero pretium in. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. Nunc id enim sed magna efficitur volutpat sit amet ac ex. ",
+            false
+        );
+        var fos = new FileOutputStream("monthlyReportBreakSignatures.pdf");
         fos.write(MonthlyReportPdf.generate(data));
         fos.close();
     }
