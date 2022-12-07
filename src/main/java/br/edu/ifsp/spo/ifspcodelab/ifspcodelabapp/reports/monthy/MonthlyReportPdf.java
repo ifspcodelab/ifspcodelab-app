93d16f300df2d2fa5ecfcaf18e0136d62ec57c2e
Domingos Latorre <domingos.latorre@gmail.com>
2022-12-07 10:16:19 -0300

    feat: add monthy report pdf generator poc

---
+++ src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/reports/monthy/MonthlyReportPdf.java
@@ -0,0 +1,121 @@
+package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports.monthy;
+
+import java.awt.Color;
+import java.io.ByteArrayOutputStream;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports.ReportTemplates;
+
+public class MonthlyReportPdf {
+    private MonthlyReportPdf() {}
+
+    public static byte[] generate(MonthlyReportData data) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            Document doc = ReportTemplates.reportDocument();
+            PdfWriter.getInstance(doc, baos);
+            doc.open();
+
+            // Header
+            if(data.volunteer) {
+                doc.add(ReportTemplates.volunteerReportHeader());
+            } else {
+                doc.add(ReportTemplates.scholarshipReportHeader());
+            }
+
+            // Titles
+            doc.add(ReportTemplates.generateTitle("ANEXO IV"));
+            doc.add(ReportTemplates.generateTitle("Relatório Mensal de Frequência e Avaliação - 2022"));
+
+            // Project, coordinator and student names
+            doc.add(generateFirstTable(data));
+
+            // Last month paragraph
+            String month = data.getDate().getMonth().minus(1).getDisplayName(TextStyle.FULL, new Locale("pt", "BR"));
+            doc.add(ReportTemplates.generateTitle("Resumo das atividades desenvolvidas no mês de " + month + "/2022", ReportTemplates.fontTitle));
+
+            // Content
+            doc.add(generateContent(data));
+
+            // Footer message
+            String message = "Observação: Entregar este relatório via plataforma Moodle até o dia 05 de cada mês, conforme previsto no edital.";
+            Paragraph messageParagraph = new Paragraph(message, ReportTemplates.fontDefault);
+            messageParagraph.setSpacingAfter(6);
+            doc.add(messageParagraph);
+
+            // Signatures
+            doc.add(generateSignatures(data));
+
+            doc.close();
+            return baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static PdfPTable generateFirstTable(MonthlyReportData data) throws Exception {
+        PdfPTable table = new PdfPTable(3);
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(15);
+        table.setSpacingAfter(8);
+        table.getDefaultCell().setBorderColor(new Color(255, 0, 0));
+        table.addCell(ReportTemplates.generateCell("Título do Projeto", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getProjectTitle(), 2, ReportTemplates.fontDefault));
+        table.addCell(ReportTemplates.generateCell("Professor Responsável", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getCoordinatorName(), 2, ReportTemplates.fontDefault));
+        
+        if(data.volunteer) {
+            table.addCell(ReportTemplates.generateCell("Voluntário", 1, ReportTemplates.fontTimesBold));
+        } else {
+            table.addCell(ReportTemplates.generateCell("Bolsista", 1, ReportTemplates.fontTimesBold));
+        }
+
+        table.addCell(ReportTemplates.generateCell(data.getStudentName(), 2, ReportTemplates.fontDefault));
+        table.addCell(ReportTemplates.generateCell("Data de entrega", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getDate().format(ReportTemplates.formatter), 2, ReportTemplates.fontDefault));
+        return table;
+    }
+
+    private static PdfPTable generateContent(MonthlyReportData data) throws Exception {
+        PdfPTable table = new PdfPTable(1);
+       
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(15);
+        table.setSpacingAfter(15);
+        table.getDefaultCell().setBorderColor(new Color(255, 0, 0));
+        table.addCell(ReportTemplates.generateCell("Atividades planejadas:", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getPlanActivities(), 1, ReportTemplates.fontDefault));
+        table.addCell(ReportTemplates.generateCell("Atividades realizadas:", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getExecutedActivities(), 1, ReportTemplates.fontDefault));
+        table.addCell(ReportTemplates.generateCell("Resultados obtidos:", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCell(data.getResults(), 1, ReportTemplates.fontDefault));
+       
+        return table;
+    }
+
+   private static PdfPTable generateSignatures(MonthlyReportData data) {
+        PdfPTable table = new PdfPTable(2);
+       
+        table.setWidthPercentage(100);
+        table.setSpacingBefore(40);
+        table.getDefaultCell().setBorderColor(new Color(255, 255, 255));
+        table.addCell(ReportTemplates.generateCellCenter("________________________________", 1, ReportTemplates.fontTimesBold));
+        table.addCell(ReportTemplates.generateCellCenter("________________________________", 1, ReportTemplates.fontTimesBold));
+
+        if(data.volunteer) {
+            table.addCell(ReportTemplates.generateCellCenter("Voluntário", 1, ReportTemplates.fontDefault));
+        } else {
+            table.addCell(ReportTemplates.generateCellCenter("Bolsista", 1, ReportTemplates.fontDefault));
+        }
+
+        table.addCell(ReportTemplates.generateCellCenter("Professor Responsável", 1, ReportTemplates.fontDefault));
+       
+        return table;
+   }
+}
--- src/test/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/reports/ReportTest.java
+++ src/test/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/reports/ReportTest.java
@@ -1,18 +1,26 @@
 package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import com.lowagie.text.BadElementException;
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 
+import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports.monthy.MonthlyReportData;
+import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.reports.monthy.MonthlyReportPdf;
 import lombok.var;
 
 public class ReportTest {
+    private String name;
     
     @Test
     public void helloWorldPdf() {
@@ -31,6 +39,54 @@ public class ReportTest {
             doc.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void headerTest() throws IOException {
+        var fos = new FileOutputStream("header-test.pdf");
+        var doc = new Document();  
+        PdfWriter.getInstance(doc, fos);
+        
+        doc.open();
+        
+        doc.add(ReportTemplates.volunteerReportHeader());
+        
+        doc.close();
+        fos.close();
+    }
+
+    @Test
+    public void titleTest() throws BadElementException, DocumentException, IOException  {
+        var fos = new FileOutputStream("title-test.pdf");
+        var doc = new Document();  
+        PdfWriter.getInstance(doc, fos);
+        
+        doc.open();
+        
+        doc.add(ReportTemplates.volunteerReportHeader());
+        doc.add(ReportTemplates.generateTitle("ANEXO IV"));
+        doc.add(ReportTemplates.generateTitle("Relatório Mensal de Frequência e Avaliação - 2022"));
+        
+        doc.close();
+        fos.close();
+    }
+
+    @Test
+    public void volunteerMonthlyReport() throws BadElementException, DocumentException, IOException  {
+        var data = new MonthlyReportData(
+            "IFSP Codelab", 
+            "João da Silva", 
+            "Maria Campus Gomes", 
+            LocalDate.now(), 
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. In condimentum purus orci, eu viverra libero pretium in. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. Nunc id enim sed magna efficitur volutpat sit amet ac ex. ",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. In condimentum purus orci, eu viverra libero pretium in. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. Nunc id enim sed magna efficitur volutpat sit amet ac ex. ",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. In condimentum purus orci, eu viverra libero pretium in. Nunc id enim sed magna efficitur volutpat sit amet ac ex. Nam in dignissim ligula, quis consectetur est. Vestibulum accumsan id metus sit amet bibendum. Praesent mauris ipsum, vestibulum quis nisl eget, cursus pretium orci. Nunc id enim sed magna efficitur volutpat sit amet ac ex. ",
+            true
+        );
+
+        try (var fos = new FileOutputStream("volunteerMonthlyReport.pdf")) {
+            fos.write(MonthlyReportPdf.generate(data));
         }
     }
 }
