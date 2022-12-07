786fa1c96600c38f93bd3d4ddf4b8071b9eef192
Lucas Gabriel <lucasogjc@gmail.com>
2022-12-07 17:31:44 -0300

    feat: adds student migration

---
+++ src/main/resources/db/migration/V3__create_students_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students(
+    id UUID NOT NULL,
+    name VARCHAR NOT NULL,
+    cpf VARCHAR NOT NULL,
+    rg VARCHAR NOT NULL,
+    birth_date DATE NOT NULL,
+    registration VARCHAR NOT NULL,
+    course_id UUID NOT NULL,
+    cellphone VARCHAR NOT NULL,
+    CONSTRAINT students_fk PRIMARY KEY (id),
+    CONSTRAINT courses_fk FOREIGN KEY (course_id) REFERENCES courses(id)
+);
