71b49b01fc1d951c9bc7d1c8cdce01f149435469
rafaelspa <rsdepaula67@gmail.com>
2022-12-07 16:54:26 -0300

    feat: web controller and security config

---
+++ src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/authentication/SecurityConfig.java
@@ -0,0 +1,37 @@
+package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorizeConfig -> {
+                    authorizeConfig.requestMatchers("/private").authenticated();
+                    authorizeConfig.anyRequest().permitAll();
+                })
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password("{noop}password")
+                        .authorities("ROLE_USER")
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/authentication/WebController.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.authentication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    @GetMapping("/")
+    public String publicPage() {
+        return "Hello world";
+    }
+
+    @GetMapping("/private")
+    public String privatePage() {
+        return "Isso deveria ser privado ";
+    }
+}
