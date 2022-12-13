153caedc4bc69ac94e75346f531c001097cf627b
Lucas Gabriel <lucasogjc@gmail.com>
2022-12-13 18:03:52 -0300

    feat: adds course list on participation form

--- src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/student/StudentController.java
+++ src/main/java/br/edu/ifsp/spo/ifspcodelab/ifspcodelabapp/student/StudentController.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.student;
 
 import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.application.Application;
 import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.application.ApplicationRepository;
-import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.application.ApplicationSelectionStatus;
+import br.edu.ifsp.spo.ifspcodelab.ifspcodelabapp.student.course.CourseRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,6 +21,7 @@ import java.util.UUID;
 @Slf4j
 public class StudentController {
     private final ApplicationRepository applicationRepository;
+    private final CourseRepository courseRepository;
     private final StudentService studentService;
 
     // http://localhost:8080/applications/26be9581-43c2-4ba9-9bf3-55edb095a362/finish-project-application
@@ -34,8 +35,13 @@ public class StudentController {
 
         ModelAndView mv = new ModelAndView("student/participation-form");
 
+        var courseList = courseRepository.findAll();
+        var applicationCourse = courseList.stream().filter(c -> c.getId().equals(application.get().getCourse().getId())).findAny().orElse(null);
+        courseList.remove(applicationCourse);
+
         //Attribute name should not be named 'application' because it conflicts with Thymeleaf
         mv.addObject("studentApplication", application.get());
+        mv.addObject("courseList", courseList);
 
         return mv;
     }
